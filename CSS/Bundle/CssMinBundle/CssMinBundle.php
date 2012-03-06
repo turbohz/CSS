a942d44673135030074596c6e0e4c714f343d231
Gerard Ruiz <gerard@quickyform.com>
2012-03-06 13:04:02 +0100

    House cleaning

--- CSS/Bundle/CssMinBundle/CssMinBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CSS\Bundle\CssMinBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CssMinBundle extends Bundle
-{	
-}
--- CSS/Bundle/LessPHPBundle/LessPHPBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CSS\Bundle\LessPHPBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LessPHPBundle extends Bundle
-{	
-}
